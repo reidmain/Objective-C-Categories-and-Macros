563be197d2cea7263e38f1446b7c5f0f6483868f
Reid Main <reidmain@gmail.com>
2012-07-04 22:15:47 -0400

    Added NSDictionary category with method that prevents NSNull objects from being returned.

---
+++ NSDictionary+Accessing.h
@@ -0,0 +1,12 @@
+#pragma mark Class Interface
+
+@interface NSDictionary (Accessing)
+
+
+#pragma mark -
+#pragma mark Instance Methods
+
+- (id)nonNullObjectForKey: (id)key;
+
+
+@end // @interface NSDictionary (Accessing)
---
+++ NSDictionary+Accessing.m
@@ -0,0 +1,25 @@
+#import "NSDictionary+Accessing.h"
+#import "FDNullOrEmpty.h"
+
+
+#pragma mark Class Definition
+
+@implementation NSDictionary (Accessing)
+
+#pragma mark -
+#pragma mark Public Methods
+
+- (id)nonNullObjectForKey: (id)key
+{
+	id object = [self objectForKey: key];
+	
+	if (FDIsNull(object) == YES)
+	{
+		object = nil;
+	}
+	
+	return object;
+}
+
+
+@end // @implementation NSDictionary (Accessing)
