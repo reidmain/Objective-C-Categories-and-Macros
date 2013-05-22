f06b2a9e1ad3fcb62c7956b28ff0c3ee19063df2
Reid Main <reidmain@gmail.com>
2013-05-22 17:24:10 -0400

    Added setPixelSnappedFrame and setPixelSnappedCenter methods.
    If you called setXOrigin and setYOrigin in the same animation block the
    call to setXOrigin would be clobbered by the call to setYOrigin because
    the both modify the fame.
    
    Two new methods have been added that will allow you to set the frame or
    center of a UIView all at once so you can modify multiple values and
    not break your animations.
    These methods will also automatically floor the needed values to
    prevent subpixel rendering.

--- UIView+Layout.h
+++ UIView+Layout.h
@@ -15,23 +15,25 @@ typedef enum
 } UIViewVerticalAlignment;
 
 
-#pragma mark -
-#pragma mark Class Interface
+#pragma mark - Class Interface
 
 @interface UIView (Layout)
 
 
-#pragma mark -
-#pragma mark Properties
+#pragma mark - Properties
 
 @property (nonatomic, assign) CGFloat xOrigin;
 @property (nonatomic, assign) CGFloat yOrigin;
 @property (nonatomic, assign) CGFloat width;
 @property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGFloat xCenter;
+@property (nonatomic, assign) CGFloat yCenter;
 
 
-#pragma mark -
-#pragma mark Instance Methods
+#pragma mark - Instance Methods
+
+- (void)setPixelSnappedFrame: (CGRect)frame;
+- (void)setPixelSnappedCenter: (CGPoint)center;
 
 - (void)alignHorizontally: (UIViewHorizontalAlignment)horizontalAlignment;
 - (void)alignVertically: (UIViewVerticalAlignment)verticalAlignment;
@@ -41,4 +43,4 @@ typedef enum
 - (void)removeAllSubviews;
 
 
-@end // @interface UIView (Layout)
+@end
--- UIView+Layout.m
+++ UIView+Layout.m
@@ -6,8 +6,7 @@
 @implementation UIView (Layout)
 
 
-#pragma mark -
-#pragma mark Properties
+#pragma mark - Properties
 
 - (CGFloat)xOrigin
 {
@@ -57,7 +56,7 @@
 	
 	CGRect viewBounds = self.bounds;
 	
-	// Floor the width to avoid subpixel rendering
+	// Floor the width to avoid subpixel rendering.
 	viewBounds.size.width = floorf(width);
 	
 	self.bounds = viewBounds;
@@ -87,12 +86,79 @@
 	self.yOrigin = previousYOrigin;
 }
 
+- (CGFloat)xCenter
+{
+	CGFloat xCenter = self.center.x;
+	
+	return xCenter;
+}
 
-#pragma mark -
-#pragma mark Public Methods
+- (void)setXCenter: (CGFloat)xCenter
+{
+	CGPoint center = self.center;
+	
+	// Calculate the x origin of the view around the new center point and floor it to avoid the sub pixel rendering that will occur if the width is an odd number.
+	CGFloat xOrigin = floorf(xCenter - (self.width / 2.0f));
+	
+	center.x = xOrigin + (self.width / 2.0f);
+	
+	self.center = center;
+}
+
+- (CGFloat)yCenter
+{
+	CGFloat yCenter = self.center.y;
+	
+	return yCenter;
+}
+
+- (void)setYCenter: (CGFloat)yCenter
+{
+	CGPoint center = self.center;
+	
+	// Calculate the y origin of the view around the new center point and floor it to avoid the sub pixel rendering that will occur if the height is an odd number.
+	CGFloat yOrigin = floorf(yCenter - (self.height / 2.0f));
+	
+	center.y = yOrigin + (self.height / 2.0f);
+	
+	self.center = center;
+}
+
+
+#pragma mark - Public Methods
+
+- (void)setPixelSnappedFrame: (CGRect)frame
+{
+	// Floor the width and height of the frame to avoid subpixel rendering.
+	CGRect bounds = CGRectMake(0.0f, 0.0f, floorf(frame.size.width), floorf(frame.size.height));
+	
+	// Floor the x and y origin of the frame to avoid subpixel rendering.
+	CGPoint center = CGPointZero;
+	center.x = floor(frame.origin.x) + (bounds.size.width / 2.0f);
+	center.y = floorf(frame.origin.y) + (bounds.size.height / 2.0f);
+	
+	self.bounds = bounds;
+	self.center = center;
+}
+
+- (void)setPixelSnappedCenter: (CGPoint)center
+{
+	// Calculate the x origin of the view around the new center point and floor it to avoid the sub pixel rendering that will occur if the width is an odd number.
+	CGFloat xOrigin = floorf(center.x - (self.width / 2.0f));
+	
+	// Calculate the y origin of the view around the new center point and floor it to avoid the sub pixel rendering that will occur if the height is an odd number.
+	CGFloat yOrigin = floorf(center.y - (self.height / 2.0f));
+	
+	// Calculate the new center of the view around the floored x and y origins.
+	center.x = xOrigin + (self.width / 2.0f);
+	center.y = yOrigin + (self.height / 2.0f);
+	
+	self.center = center;
+}
 
 - (void)alignHorizontally: (UIViewHorizontalAlignment)horizontalAlignment
 {
+	// If the view does not have a superview there is nothing to align it against.
 	if (self.superview == nil)
 	{
 		return;
@@ -125,6 +191,7 @@
 
 - (void)alignVertically: (UIViewVerticalAlignment)verticalAlignment
 {
+	// If the view does not have a superview there is nothing to align it against.
 	if (self.superview == nil)
 	{
 		return;
@@ -158,8 +225,66 @@
 - (void)alignHorizontally: (UIViewHorizontalAlignment)horizontalAlignment
 	vertically: (UIViewVerticalAlignment)verticalAlignment
 {
-	[self alignHorizontally: horizontalAlignment];
-	[self alignVertically: verticalAlignment];
+	// If the view does not have a superview there is nothing to align it against.
+	if (self.superview == nil)
+	{
+		return;
+	}
+	
+	CGFloat xOrigin = 0.0f;
+	CGFloat yOrigin = 0.0f;
+	
+	switch (horizontalAlignment)
+	{
+		case UIViewHorizontalAlignmentCenter:
+		{
+			xOrigin = (self.superview.width - self.width) / 2.0f;
+			
+			break;
+		}
+		
+		case UIViewHorizontalAlignmentLeft:
+		{
+			xOrigin = 0.0f;
+			
+			break;
+		}
+		
+		case UIViewHorizontalAlignmentRight:
+		{
+			xOrigin = self.superview.width - self.width;
+			
+			break;
+		}
+	}
+	
+	switch (verticalAlignment)
+	{
+		case UIViewVerticalAlignmentMiddle:
+		{
+			yOrigin = (self.superview.height - self.height) / 2.0f;
+			
+			break;
+		}
+		
+		case UIViewVerticalAlignmentTop:
+		{
+			yOrigin = 0.0;
+			
+			break;
+		}
+		
+		case UIViewVerticalAlignmentBottom:
+		{
+			yOrigin = self.superview.height - self.height;
+			
+			break;
+		}
+	}
+	
+	CGRect frame = CGRectMake(xOrigin, yOrigin, self.width, self.height);
+	
+	[self setPixelSnappedFrame: frame];
 }
 
 - (void)removeAllSubviews
@@ -168,4 +293,4 @@
 }
 
 
-@end // @implementation UIView (Layout)
+@end
